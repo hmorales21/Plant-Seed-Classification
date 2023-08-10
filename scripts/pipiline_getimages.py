36d7556ffe671b251c18a8e64dfdeb860207c631
Horacio Morales <55999975+hmorales21@users.noreply.github.com>
2023-08-09 19:48:43 -0600

    modelling ok

--- scripts/pipiline_getimages.py
+++ scripts/pipiline_getimages.py
@@ -3,7 +3,7 @@ import pathlib
 import tensorflow as tf
 
 dataset_url = "https://vision.eng.au.dk/?download=/data/WeedData/NonsegmentedV2.zip"
-data_dir = tf.keras.utils.get_file('NonsegmentedV2.zip', origin=dataset_url, extract=True)
+data_dir = tf.keras.utils.get_file('../datasets/NonsegmentedV2.zip', origin=dataset_url, extract=True)
 data_dir = pathlib.Path(data_dir).with_suffix('')
 
 image_count = len(list(data_dir.glob('*/*.jpg')))
